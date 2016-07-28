4a06303658e7da9227a195c32b7c6eff4f3bb03f
IbrahimSassi <sassiibrahim12@gmail.com>
2016-07-28 22:33:40 +0100

    First Commit

---
+++ textcircle.js
@@ -0,0 +1,45 @@
+
+this.Documents = new Mongo.Collection("documents");
+
+if (Meteor.isClient){
+    
+    
+    
+    Meteor.setInterval(function(){
+        Session.set("current_date",new Date());
+    },1000);
+    
+    Template.date_display.helpers({
+        current_date:function(){
+            return Session.get("current_date");
+        }
+    });
+    
+    
+    Template.editor.helpers({
+        docid:function(){
+            console.log(Documents.findOne());
+            
+            var doc = Documents.findOne();
+            
+            if(doc){
+            return doc._id;
+            }
+            else
+                {
+                    return undefined;
+                }
+        }
+    });
+}
+
+if (Meteor.isServer){
+	Meteor.startup(function(){
+		// code to run on server at startup
+        if (!Documents.findOne())
+            {
+                Documents.insert({title:"my new document"});
+                
+            }
+	})
+}
